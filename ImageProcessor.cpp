4fbc6383f9e8d6ddb77f81baecc6507fcf554965
dli34 <dli34@stevens.edu>
2020-05-11 18:04:12 -0400

    Added basic low pass filter function.

--- ImageProcessor.cpp
+++ ImageProcessor.cpp
@@ -27,7 +27,6 @@ uint32_t ImageProcessor::getSize()
 // High-pass Filter
 void ImageProcessor::highPassFilter()
 {
-
 	int m = 0, n = 0, o = 0, p = 0, q = 0;
 	for (int j = 0; j < height; j++) {
 		for (int k = 0; k < width; k++) {
@@ -54,10 +53,35 @@ void ImageProcessor::highPassFilter()
 			image_out[j][k] = m;
 		}
 	}
+}
 
-	//test
-
-	
+void ImageProcessor::lowPassFilter()
+{
+	int m = 0, n = 0, o = 0, p = 0, q = 0;
+	for (j = 0; j < height; j++) {
+		for (k = 0; k < width; k++) {
+			m = image_in[j][k] * -3;
+			if (j + 1 < height) {
+				n = image_in[j + 1][k] * (1);
+			}
+			if (j - 1 > 0) {
+				o = image_in[j - 1][k] * (1);
+			}
+			if (k + 1 < width) {
+				p = image_in[j][k + 1] * (1);
+			}
+			if (k - 1 > 0) {
+				q = image_in[j][k - 1] * (1);
+			}
+			m = m + n + o + p + q;
+			if (m > 255) {
+				m = 255;
+			}
+			else if (m < 0) {
+				m = 0;
+			}
+			image_out[j][k] = m;
+		}
 }
 
 int main(int argc, char *argv[])
