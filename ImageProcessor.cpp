100fae6e97b6a944384148cba83e714dac943aa3
dli34 <46940842+dli34@users.noreply.github.com>
2020-05-11 16:35:48 -0400

    added high pass filter. Brute forced, but can be changed later.

--- ImageProcessor.cpp
+++ ImageProcessor.cpp
@@ -19,6 +19,31 @@ uint32_t ImageProcessor::getSize()
 
 void ImageProcessor::highPassFilter(Image& img_in)
 {
-	
+	int m = 0, n = 0, o = 0, p = 0, q = 0;
+	for (int j = 0; j < height; j++) {
+		for (int k = 0; k < width; k++) {
+			m = image_in[j][k] * 5;
+			if (j + 1 < height) {
+				n = image_in[j + 1][k] * (-1);
+			}
+			if (j - 1 > 0) {
+				o = image_in[j - 1][k] * (-1);
+			}
+			if (k + 1 < width) {
+				p = image_in[j][k + 1] * (-1);
+			}
+			if (k - 1 > 0) {
+				q = image_in[j][k - 1] * (-1);
+			}
+			m = m + n + o + p + q;
+			if (m > 255) {
+				m = 255;
+			}
+			else if (m < 0) {
+				m = 0;
+			}
+			image_out[j][k] = m;
+		}
+	}
 	
 }
